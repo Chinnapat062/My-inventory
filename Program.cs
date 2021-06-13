6092e7c63a2dc95de5bb0e9248ef90b299d8bd2c
Chinnapat062 <78645438+Chinnapat062@users.noreply.github.com>
2021-06-13 17:12:38 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace My_inventory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("how many item");
+            int itemnum = int.Parse(Console.ReadLine());
+            string[] itemname = new string[itemnum];
+            for (int i = 1; i <= itemname.Length; i++)
+            {
+                string name = Console.ReadLine();
+                string typeitem = Console.ReadLine();
+                string finditem = Console.ReadLine();
+                if (finditem == "ShowAll")
+                {
+                    Console.WriteLine(name, typeitem);
+                    return;
+                }
+                else if (finditem == typeitem)
+                {
+                    Console.WriteLine(name);
+                }
+                else
+                {
+                    Console.WriteLine("End");
+                }
+                Console.ReadKey();
+
+            }
+            
+            
+
+
+        }
+        
+            
+        
+        
+    }
+}
